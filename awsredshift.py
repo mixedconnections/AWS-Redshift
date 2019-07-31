26fcaaddb5e91d7cb88bf448412e73f9c1ec783c
Mike Accardo <accardo@cpan.org>
2019-07-31 14:38:31 -0400

    applied autopep8

--- awsredshift.py
+++ awsredshift.py
@@ -1,23 +1,24 @@
 #!/usr/bin/env python
 
-# Creating Redshift Cluster using the AWS python SDK 
+# Creating Redshift Cluster using the AWS python SDK
 # ## An example of Infrastructure-as-code
 
+from botocore.exceptions import ClientError
 import pandas as pd
 import boto3
 import json
 
 
 # # STEP 0: Make sure you have an AWS secret and access key
-# 
+#
 # - Create a new IAM user in your AWS account
 # - Give it `AdministratorAccess`, From `Attach existing policies directly` Tab
-# - Take note of the access key and secret 
+# - Take note of the access key and secret
 # - Edit the file `dwh.cfg` in the same directory as this file
 # [AWS]<BR>
 # KEY= YOUR_AWS_KEY<BR>
 # SECRET= YOUR_AWS_SECRET<BR>
-# 
+#
 
 # # Load DWH Params from a file
 
@@ -25,61 +26,73 @@ import configparser
 config = configparser.ConfigParser()
 config.read_file(open('dwh.cfg'))
 
-KEY                    = config.get('AWS','KEY')
-SECRET                 = config.get('AWS','SECRET')
+KEY = config.get('AWS', 'KEY')
+SECRET = config.get('AWS', 'SECRET')
 
-DWH_CLUSTER_TYPE       = config.get("DWH","DWH_CLUSTER_TYPE")
-DWH_NUM_NODES          = config.get("DWH","DWH_NUM_NODES")
-DWH_NODE_TYPE          = config.get("DWH","DWH_NODE_TYPE")
+DWH_CLUSTER_TYPE = config.get("DWH", "DWH_CLUSTER_TYPE")
+DWH_NUM_NODES = config.get("DWH", "DWH_NUM_NODES")
+DWH_NODE_TYPE = config.get("DWH", "DWH_NODE_TYPE")
 
-DWH_CLUSTER_IDENTIFIER = config.get("DWH","DWH_CLUSTER_IDENTIFIER")
-DWH_DB                 = config.get("DWH","DWH_DB")
-DWH_DB_USER            = config.get("DWH","DWH_DB_USER")
-DWH_DB_PASSWORD        = config.get("DWH","DWH_DB_PASSWORD")
-DWH_PORT               = config.get("DWH","DWH_PORT")
+DWH_CLUSTER_IDENTIFIER = config.get("DWH", "DWH_CLUSTER_IDENTIFIER")
+DWH_DB = config.get("DWH", "DWH_DB")
+DWH_DB_USER = config.get("DWH", "DWH_DB_USER")
+DWH_DB_PASSWORD = config.get("DWH", "DWH_DB_PASSWORD")
+DWH_PORT = config.get("DWH", "DWH_PORT")
 
-DWH_IAM_ROLE_NAME      = config.get("DWH", "DWH_IAM_ROLE_NAME")
+DWH_IAM_ROLE_NAME = config.get("DWH", "DWH_IAM_ROLE_NAME")
 
 (DWH_DB_USER, DWH_DB_PASSWORD, DWH_DB)
 
-pd.DataFrame({"Param":
-                  ["DWH_CLUSTER_TYPE", "DWH_NUM_NODES", "DWH_NODE_TYPE", "DWH_CLUSTER_IDENTIFIER", "DWH_DB", "DWH_DB_USER", "DWH_DB_PASSWORD", "DWH_PORT", "DWH_IAM_ROLE_NAME"],
-              "Value":
-                  [DWH_CLUSTER_TYPE, DWH_NUM_NODES, DWH_NODE_TYPE, DWH_CLUSTER_IDENTIFIER, DWH_DB, DWH_DB_USER, DWH_DB_PASSWORD, DWH_PORT, DWH_IAM_ROLE_NAME]
-             })
+pd.DataFrame({"Param": ["DWH_CLUSTER_TYPE",
+                        "DWH_NUM_NODES",
+                        "DWH_NODE_TYPE",
+                        "DWH_CLUSTER_IDENTIFIER",
+                        "DWH_DB",
+                        "DWH_DB_USER",
+                        "DWH_DB_PASSWORD",
+                        "DWH_PORT",
+                        "DWH_IAM_ROLE_NAME"],
+              "Value": [DWH_CLUSTER_TYPE,
+                        DWH_NUM_NODES,
+                        DWH_NODE_TYPE,
+                        DWH_CLUSTER_IDENTIFIER,
+                        DWH_DB,
+                        DWH_DB_USER,
+                        DWH_DB_PASSWORD,
+                        DWH_PORT,
+                        DWH_IAM_ROLE_NAME]})
 
 
 # # Create clients for IAM, EC2, S3 and Redshift
 
-import boto3
 
 ec2 = boto3.resource('ec2',
-                       region_name="us-west-2",
-                       aws_access_key_id=KEY,
-                       aws_secret_access_key=SECRET
-                    )
+                     region_name="us-west-2",
+                     aws_access_key_id=KEY,
+                     aws_secret_access_key=SECRET
+                     )
 
 s3 = boto3.resource('s3',
-                       region_name="us-west-2",
-                       aws_access_key_id=KEY,
-                       aws_secret_access_key=SECRET
+                    region_name="us-west-2",
+                    aws_access_key_id=KEY,
+                    aws_secret_access_key=SECRET
+                    )
+
+iam = boto3.client('iam', aws_access_key_id=KEY,
+                   aws_secret_access_key=SECRET,
+                   region_name='us-west-2'
                    )
 
-iam = boto3.client('iam',aws_access_key_id=KEY,
-                     aws_secret_access_key=SECRET,
-                     region_name='us-west-2'
-                  )
-
 redshift = boto3.client('redshift',
-                       region_name="us-west-2",
-                       aws_access_key_id=KEY,
-                       aws_secret_access_key=SECRET
-                       )
+                        region_name="us-west-2",
+                        aws_access_key_id=KEY,
+                        aws_secret_access_key=SECRET
+                        )
 
 
 # # Check out the sample data sources on S3
 
-sampleDbBucket =  s3.Bucket("awssampledbuswest2")
+sampleDbBucket = s3.Bucket("awssampledbuswest2")
 for obj in sampleDbBucket.objects.filter(Prefix="ssbgz"):
     print(obj)
 # for obj in sampleDbBucket.objects.all():
@@ -89,30 +102,29 @@ for obj in sampleDbBucket.objects.filter(Prefix="ssbgz"):
 # # STEP 1: IAM ROLE
 # - Create an IAM Role that makes Redshift able to access S3 bucket (ReadOnly)
 
-from botocore.exceptions import ClientError
 
-#1.1 Create the role, 
+# 1.1 Create the role,
 try:
-    print("1.1 Creating a new IAM Role") 
+    print("1.1 Creating a new IAM Role")
     dwhRole = iam.create_role(
         Path='/',
         RoleName=DWH_IAM_ROLE_NAME,
-        Description = "Allows Redshift clusters to call AWS services on your behalf.",
+        Description="Allows Redshift clusters to call AWS services on your behalf.",
         AssumeRolePolicyDocument=json.dumps(
             {'Statement': [{'Action': 'sts:AssumeRole',
-               'Effect': 'Allow',
-               'Principal': {'Service': 'redshift.amazonaws.com'}}],
+                            'Effect': 'Allow',
+                            'Principal': {'Service': 'redshift.amazonaws.com'}}],
              'Version': '2012-10-17'})
-    )    
+    )
 except Exception as e:
     print(e)
-    
-    
+
+
 print("1.2 Attaching Policy")
 
 iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
                        PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-                      )['ResponseMetadata']['HTTPStatusCode']
+                       )['ResponseMetadata']['HTTPStatusCode']
 
 print("1.3 Get the IAM role ARN")
 roleArn = iam.get_role(RoleName=DWH_IAM_ROLE_NAME)['Role']['Arn']
@@ -121,13 +133,13 @@ print(roleArn)
 
 
 # # STEP 2:  Redshift Cluster
-# 
+#
 # - Create a RedShift Cluster
 # - For complete arguments to `create_cluster`, see [docs](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/redshift.html#Redshift.Client.create_cluster)
 
 try:
-    response = redshift.create_cluster(        
-        #HW
+    response = redshift.create_cluster(
+        # HW
         ClusterType=DWH_CLUSTER_TYPE,
         NodeType=DWH_NODE_TYPE,
         NumberOfNodes=int(DWH_NUM_NODES),
@@ -137,9 +149,9 @@ try:
         ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
         MasterUsername=DWH_DB_USER,
         MasterUserPassword=DWH_DB_PASSWORD,
-        
-        #Roles (for s3 access)
-        IamRoles=[roleArn]  
+
+        # Roles (for s3 access)
+        IamRoles=[roleArn]
     )
 except Exception as e:
     print(e)
@@ -150,17 +162,28 @@ except Exception as e:
 
 def prettyRedshiftProps(props):
     pd.set_option('display.max_colwidth', -1)
-    keysToShow = ["ClusterIdentifier", "NodeType", "ClusterStatus", "MasterUsername", "MasterUserPassword", "DBName", "Endpoint", "NumberOfNodes", 'VpcId'] 
-    x = [(k, v) for k,v in props.items() if k in keysToShow]
+    keysToShow = [
+        "ClusterIdentifier",
+        "NodeType",
+        "ClusterStatus",
+        "MasterUsername",
+        "MasterUserPassword",
+        "DBName",
+        "Endpoint",
+        "NumberOfNodes",
+        'VpcId']
+    x = [(k, v) for k, v in props.items() if k in keysToShow]
     return pd.DataFrame(data=x, columns=["Key", "Value"])
 
-myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+
+myClusterProps = redshift.describe_clusters(
+    ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
 prettyRedshiftProps(myClusterProps)
 
 
-# 2.2 Take note of the cluster endpoint and role ARN 
+# 2.2 Take note of the cluster endpoint and role ARN
 
-# DO NOT RUN THIS unless the cluster status becomes "Available" 
+# DO NOT RUN THIS unless the cluster status becomes "Available"
 
 DWH_ENDPOINT = myClusterProps['Endpoint']['Address']
 DWH_ROLE_ARN = myClusterProps['IamRoles'][0]['IamRoleArn']
@@ -189,28 +212,34 @@ except Exception as e:
 
 get_ipython().run_line_magic('load_ext', 'sql')
 
-conn_string="postgresql://{}:{}@{}:{}/{}".format(DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT,DWH_DB)
+conn_string = "postgresql://{}:{}@{}:{}/{}".format(
+    DWH_DB_USER, DWH_DB_PASSWORD, DWH_ENDPOINT, DWH_PORT, DWH_DB)
 print(conn_string)
 get_ipython().run_line_magic('sql', '$conn_string')
 
 
 # # STEP 5: Clean up your resources
 
-# DO NOT RUN THIS UNLESS YOU ARE SURE  
+# DO NOT RUN THIS UNLESS YOU ARE SURE
 
-#### CAREFUL!!
-#-- Uncomment & run to delete the created resources
-redshift.delete_cluster( ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,  SkipFinalClusterSnapshot=True)
-#### CAREFUL!!
+# CAREFUL!!
+# -- Uncomment & run to delete the created resources
+redshift.delete_cluster(
+    ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+    SkipFinalClusterSnapshot=True)
+# CAREFUL!!
 
 
 # - run this block several times until the cluster really deleted
 
-myClusterProps = redshift.describe_clusters(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
+myClusterProps = redshift.describe_clusters(
+    ClusterIdentifier=DWH_CLUSTER_IDENTIFIER)['Clusters'][0]
 prettyRedshiftProps(myClusterProps)
 
-#### CAREFUL!!
-#-- Uncomment & run to delete the created resources
-iam.detach_role_policy(RoleName=DWH_IAM_ROLE_NAME, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
+# CAREFUL!!
+# -- Uncomment & run to delete the created resources
+iam.detach_role_policy(
+    RoleName=DWH_IAM_ROLE_NAME,
+    PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
 iam.delete_role(RoleName=DWH_IAM_ROLE_NAME)
-#### CAREFUL!!
+# CAREFUL!!
